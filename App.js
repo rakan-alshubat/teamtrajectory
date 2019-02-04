86b75585ba1ba8a845750060ae22a208ee1dc25b
Rakan Alshubat <rakan@rakans-mbp.3520.dhcp.asu.edu>
2019-02-04 14:39:47 -0700

    wireframe main screen ui

--- App.js
+++ App.js
@@ -1,54 +1,20 @@
-import React, { PureComponent } from 'react'
-import { AppRegistry, StyleSheet, View, Dimensions } from 'react-native'
-import { GameLoop } from 'react-native-game-engine'
+import React from 'react'
+import { Dimensions } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+import MainScreen from './MainScreen'
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get('window')
 
-export default class App extends PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      x: WIDTH / 2,
-      y: HEIGHT / 2
-    }
-  }
-
-  onUpdate ({ touches }) {
-    let move = touches.find(x => x.type === 'move')
-    if (move) {
-      this.setState({
-        x: this.state.x + move.delta.pageX,
-        y: this.state.y + move.delta.pageY
-      })
-      console.log(this.state)
-    }
-  }
-
+export default class App extends React.Component {
   render () {
-    const x = this.state.x - 20
-    const y = this.state.y - 20
     return (
-      <GameLoop style={styles.container} onUpdate={this.onUpdate.bind(this)}>
-        <View style={[styles.finger, { top: y, left: x }]} />
-      </GameLoop>
+      <AppStackNavigator />
     )
   }
 }
 
-const styles = StyleSheet.create({
-  finger: {
-    borderColor: '#CCC',
-    borderWidth: 4,
-    borderRadius: 40,
-    width: 40,
-    height: 40,
-    backgroundColor: 'black',
-    position: 'absolute'
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF'
+const AppStackNavigator = StackNavigator({
+  Main: {
+    screen: MainScreen
   }
 })
-
-AppRegistry.registerComponent('App', () => App)
---
+++ MainScreen.js
@@ -0,0 +1,82 @@
+import * as React from 'react'
+import { View, StyleSheet, Image, Button, Alert } from 'react-native'
+
+export default class MainScreen extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <View style={styles.logoBox}>
+          <Image style={styles.logo} source={require('./assets/snack-icon.png')} />
+        </View>
+        <View style={styles.startButtonBox}>
+          <Button
+            title="Start"
+            color="#ffffff"
+            onPress={() => {
+              Alert.alert('Start the game!')
+            }}
+          />
+        </View>
+        <View style={styles.upgradeButtonBox}>
+          <Button
+            title="Upgrade"
+            color="#ffffff"
+            onPress={() => {
+              Alert.alert('Go to the upgrade screen!!')
+            }}
+          />
+        </View>
+        <View style={styles.infoBox}>
+          <Button
+            title="i"
+            color="#ffffff"
+            onPress={() => {
+              Alert.alert('Info about the game and the app')
+            }}
+          />
+          <Button
+            title="Settings"
+            color="#ffffff"
+            onPress={() => {
+              Alert.alert('settings')
+            }}
+          />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logoBox: {
+    flex: 5,
+    backgroundColor: '#3D9ECF',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  startButtonBox: {
+    flex: 1,
+    backgroundColor: '#3D9ECF',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  upgradeButtonBox: {
+    flex: 2,
+    backgroundColor: '#3D9ECF',
+    alignItems: 'center'
+  },
+  infoBox: {
+    flex: 1,
+    backgroundColor: '#3D9ECF',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  logo: {
+    height: 164,
+    width: 164
+  }
+})
