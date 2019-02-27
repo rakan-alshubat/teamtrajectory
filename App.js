d2f35b77b91b60d3ca4ea4d473631cffd6f2e4e8
Rakan Alshubat <ralshuba@asu.edu>
2019-02-27 14:58:11 -0700

    test

--- App.js
+++ App.js
@@ -1,54 +1,10 @@
 import React, { PureComponent } from 'react'
-import { AppRegistry, StyleSheet, View, Dimensions } from 'react-native'
-import { GameLoop } from 'react-native-game-engine'
-
-const { width: WIDTH, height: HEIGHT } = Dimensions.get('window')
+import Obstacle from './src/components/Obstacle'
 
 export default class App extends PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      x: WIDTH / 2,
-      y: HEIGHT / 2
-    }
-  }
-
-  onUpdate ({ touches }) {
-    let move = touches.find(x => x.type === 'move')
-    if (move) {
-      this.setState({
-        x: this.state.x + move.delta.pageX,
-        y: this.state.y + move.delta.pageY
-      })
-      console.log(this.state)
-    }
-  }
-
   render () {
-    const x = this.state.x - 20
-    const y = this.state.y - 20
     return (
-      <GameLoop style={styles.container} onUpdate={this.onUpdate.bind(this)}>
-        <View style={[styles.finger, { top: y, left: x }]} />
-      </GameLoop>
+      <Obstacle />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  finger: {
-    borderColor: '#CCC',
-    borderWidth: 4,
-    borderRadius: 40,
-    width: 40,
-    height: 40,
-    backgroundColor: 'black',
-    position: 'absolute'
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF'
-  }
-})
-
-AppRegistry.registerComponent('App', () => App)
---
+++ src/components/Obstacle.js
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react'
+import { StyleSheet, View } from 'react-native'
+
+class Obstacle extends PureComponent {
+  render () {
+    const width = 100
+    const height = 50
+    const x = this.props.position[0] - (width / 2)
+    const y = this.props.position[1] - (height / 2)
+    return (
+      <View style={[styles.obstacle, { width, height, left: x, bottom: y }]}/>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  obstacle: {
+    position: 'absolute',
+    backgroundColor: 'grey',
+    borderRadius: 5,
+    borderWidth: 3,
+    borderColor: 'black'
+  }
+})
+
+Obstacle.propTypes = {
+  position: PropTypes.node
+}
+
+export { Obstacle }
