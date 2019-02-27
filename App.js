d40321136521244ba375b00fa09f56323813f3ec
Tristan Kimball <ttkimball1@gmail.com>
2019-02-27 14:25:50 -0700

    use game engine

--- App.js
+++ App.js
@@ -1,54 +1,26 @@
 import React, { PureComponent } from 'react'
-import { AppRegistry, StyleSheet, View, Dimensions } from 'react-native'
-import { GameLoop } from 'react-native-game-engine'
-import Ship from './src/components/Ship'
+import { AppRegistry, StyleSheet, Dimensions } from 'react-native'
+import { GameEngine } from 'react-native-game-engine'
+import { Ship } from './src/components/Ship'
+import { MoveShip } from './src/systems'
 
-const { width: WIDTH, height: HEIGHT } = Dimensions.get('window')
+const { width: WIDTH } = Dimensions.get('window')
 
 export default class App extends PureComponent {
-  constructor () {
-    super()
-    this.state = {
-      x: WIDTH / 2,
-      y: HEIGHT / 2
-    }
-  }
-
-  onUpdate ({ touches }) {
-    let move = touches.find(x => x.type === 'move')
-    if (move) {
-      this.setState({
-        x: this.state.x + move.delta.pageX,
-        y: this.state.y + move.delta.pageY
-      })
-      console.log(this.state)
-    }
-  }
-
   render () {
-    const x = this.state.x - 20
-    const y = this.state.y - 20
     return (
-      <Ship />
+      <GameEngine
+        style={styles.container}
+        systems={[MoveShip]}
+        entities={{
+          1: { position: [200, 100], renderer: <Ship /> }
+        }}>
+      </GameEngine>
     )
   }
 }
-/*
-<GameLoop style={styles.container} onUpdate={this.onUpdate.bind(this)}>
-<View style={[styles.finger, { top: y, left: x }]} />
-</GameLoop>
-*/
 
 const styles = StyleSheet.create({
-  finger: {
-    borderColor: '#CCC',
-    borderWidth: 4,
-    borderRadius: 40,
-    width: 40,
-    height: 40,
-    backgroundColor: 'black',
-    position: 'absolute'
-  },
   container: {
     flex: 1,
     backgroundColor: '#FFF'
